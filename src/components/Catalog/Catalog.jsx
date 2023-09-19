5268718ae0b792c806e7b7d470883496694a97fa
Kateryna18 <mikolaichukkate@icloud.com>
2023-09-19 23:40:46 +0300

    added list item

--- src/components/Catalog/Catalog.jsx
+++ src/components/Catalog/Catalog.jsx
@@ -3,6 +3,29 @@ import css from './Catalog.module.css';
 
 export default function Catalog() {
   return (
-    <div>Catalog</div>
-  )
+    <ul>
+      <li>
+        <div>
+          <img src="" alt="" />
+        </div>
+        <div>
+          <h2></h2>
+          <p></p>
+          <p></p>
+          <p></p>
+        </div>
+        <div>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        <button type='submit'></button>
+      </li>
+    </ul>
+  );
 }
